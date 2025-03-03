4174ebb396abb5aec7b687e46dac5e14a06e226b
daya0576 <daya0576@gmail.com>
2025-03-03 14:55:17 +0800

    Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+// Is struct similar to a class?
+// No, just have variable and function
+
+// ": View": means behaves like a ...
+// must have variable "body"
 struct ContentView: View {
+    
+    // Computed Property
+    // "some" means one of view, similar to View<?> in java
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        // "{}" embedded function -> similar to Java Anonymous Class
+        // syntax sugar:
+        // VStack(content: { ... })
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+
+        // scope matters
+        .foregroundStyle(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    // constructor variable
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                // function (view modifier)
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    // .strokeBorder(style: StrokeStyle(lineWidth: 5))
+                    .strokeBorder(lineWidth: 5)
+                
+                // .largeTitle is predefined style (css)
+                Text("😋").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
     }
 }
 
