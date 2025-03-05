d896ad5cd098f5b4b1bf41d1165e8ea14a07674f
daya0576 <daya0576@gmail.com>
2025-03-05 21:51:29 +0800

    Lecture2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -14,45 +14,84 @@ import SwiftUI
 // must have variable "body"
 struct ContentView: View {
     
+    let emojis = ["🥰", "🥳", "🥵", "😄", "❌"]
+    @State var cardCount = 4
+    
     // Computed Property
     // "some" means one of view, similar to View<?> in java
     var body: some View {
         
         // "{}" embedded function -> similar to Java Anonymous Class
-        // syntax sugar:
+        // syntax sugar: Trailing Closure Context
         // VStack(content: { ... })
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            cards
+            Spacer()
+            adjusters
         }
 
         // scope matters
         .foregroundStyle(.orange)
         .padding()
     }
+    
+    var cards: some View {
+        // Why not return?
+        // implicit return
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
+    }
+
+    func cardCountAjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1)
+        .disabled(cardCount + offset > emojis.count)
+    }
+
+    var adjusters: some View {
+        HStack {
+            cardCountAjuster(by: +1, symbol: "plus.app")
+            Spacer()
+            cardCountAjuster(by: -1, symbol: "minus.square")
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    
 }
 
 struct CardView: View {
     // constructor variable
-    var isFaceUp: Bool = false
-    
-    var body: some View {
-        ZStack {
-            if isFaceUp {
+    let content: String
+    // Pointer created
+    @State var isFaceUp = true
+
+    var body: some View    {
+        ZStack (alignment: .center) {
+            let base = RoundedRectangle(cornerRadius: 12)
+            base.fill().opacity(isFaceUp ? 0:1)
+            
+            Group {
                 // function (view modifier)
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    // .strokeBorder(style: StrokeStyle(lineWidth: 5))
-                    .strokeBorder(lineWidth: 5)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 
                 // .largeTitle is predefined style (css)
-                Text("😋").font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1: 0)
+            
+        }.onTapGesture{
+            isFaceUp = !isFaceUp
+            print("tapped: \(isFaceUp)")
         }
     }
 }
